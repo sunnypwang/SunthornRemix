96d94941d73920be8201734bd443cd94a84a3bb5
sunnypwang <mariosunshinekirby@gmail.com>
2019-04-27 09:30:31 +0000

    Implemented webpage and web app

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+
 import pickle
-import numpy as np
 from keras.models import load_model, Model
-from keras.preprocessing.sequence import pad_sequences
 import keras.backend as K
 import tensorflow as tf
-import re
+from pythainlp.tokenize import word_tokenize
+
+import prediction
 
 app = Flask(__name__)
 
@@ -15,37 +16,30 @@ graph = tf.get_default_graph()
 
 word_to_idx = pickle.load(open('res/word_to_idx.p','rb'))
 idx_to_word = pickle.load(open('res/idx_to_word.p','rb'))
-
-def texts_to_sequences(text, word_to_index):
-    text = text.strip().split()
-    token_list = [word_to_index[x] for x in text]
-    return token_list
+in_x = 44
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 @app.route('/predict')
-def predict():
-    global graph
-    with graph.as_default():
-        in_x = 44
-        current_text = request.args.get('text', default = 'ฝ่าย')
-        current_text = "<s> " + current_text
-        probs = []
-        for _ in range(in_x):
-            tmp = texts_to_sequences(current_text, word_to_idx)
-            tmp = pad_sequences([tmp], maxlen=in_x, value=0.0)
+def _predict():
+    method =  request.args.get('method', default = 'greedy')
+    input_text = request.args.get('tok', default = '')
+    wak_limit = int(request.args.get('wak', default = '4'))
+    isMobile = (request.args.get('mobile', default = 'false')).lower() == 'true'
+    
+    word_tokens = ['<s>']
+    word_tokens.extend(word_tokenize(input_text))
+    
+    if method == 'greedy':
+         result_tokens = prediction.predict_greedy(word_tokens,in_x,graph,model,word_to_idx,idx_to_word,wak_limit)
+    elif method == 'beam':
+         result_tokens = prediction.beam_search_decode(word_tokens, in_x, graph, model, word_to_idx, idx_to_word, in_x, 10, wak_limit, normalized=True)
+    result = prediction.format_output(result_tokens,wak_limit,isMobile)      
+    print(isMobile, result)
+    return result
 
-            output_word = model.predict(tmp)
-            probs.append(output_word.max())
-            output_word = np.argmax(output_word)
-            output_word = idx_to_word[output_word]
-
-            current_text += " " + output_word
-        current_text = re.sub(r'\<\/*s\> ','',current_text)
-        print(current_text)
-        return current_text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
---
+++ prediction.py
@@ -0,0 +1,106 @@
+
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+def format_output(word_tokens,wak_limit,isMobile):
+    output_text = []
+    wak_count = 0
+    for word in word_tokens:
+        if word == '/':
+            if isMobile:
+                output_text.append('<br>')
+            else:
+                wak_count += 1
+                if wak_count == 2:
+                    output_text.append('<br>')
+                else:
+                    output_text.append(' ')
+        elif word != '<s>' and word != '</s>':
+            output_text.append(word)
+    output_text = ''.join(output_text)
+    return output_text
+    
+def tokens_to_sequences(tokens, word_to_index):
+    seq = [word_to_index[tok] for tok in tokens]
+    return seq
+
+def predict_greedy(word_tokens,in_x,graph,model,word_to_idx,idx_to_word,wak_limit=4):
+    with graph.as_default():
+
+        wak_count = 0
+        
+        probs = []
+        for _ in range(in_x):
+            tmp = tokens_to_sequences(word_tokens, word_to_idx)
+            tmp = pad_sequences([tmp], maxlen=in_x, value=0.0)
+
+            output_word = model.predict(tmp)
+            probs.append(output_word.max())
+            output_word = np.argmax(output_word)
+            output_word = idx_to_word[output_word]
+
+            
+            if output_word == '/':
+                wak_count += 1
+            if wak_count < wak_limit:
+                word_tokens.append(output_word)
+            else:
+                break
+
+        return word_tokens
+    
+def cal_score(score_list, length, normalized=False):
+
+    seq_score = 0
+    tmp_length = min(length,len(score_list))
+    for i in range(length):
+
+        seq_score+=np.log(score_list[i] + 0.00000001)
+    
+    if(normalized):
+        seq_score/=length
+    
+    return np.exp(seq_score)
+
+def beam_search_decode(seed_tokens, max_gen_len, graph, model, word_to_index, index_to_word, max_sequence_len, beam_size, wak_limit=4, normalized=False):
+    with graph.as_default():
+        beams = [[seed_tokens, [], 0, 0]]
+
+        for _ in range(max_gen_len):
+
+            tmp_beams = beams.copy()
+            beams = []
+            for beam in tmp_beams:
+                if(beam[0][-1] == "</s>"):
+                    beams.append(beam)
+                    continue
+                
+                if(beam[3] >= wak_limit):
+                    beams.append(beam)
+                    continue
+                    
+                tmp = tokens_to_sequences(beam[0], word_to_index)
+                tmp = pad_sequences([tmp], maxlen=max_sequence_len, value=0.0)
+
+                output_word = model.predict(tmp).reshape(-1)
+                top_beam_idx = np.argsort(output_word)[-1*beam_size:]
+                top_beam_values = [output_word[i] for i in top_beam_idx]
+                
+                for i in range(beam_size):
+                    tmp_beam_0 = beam[0] + [index_to_word[top_beam_idx[i]]]
+                    tmp_beam_1 = beam[1] + [top_beam_values[i]]
+                    tmp_beam_2 = beam[2] + 1
+                    if index_to_word[top_beam_idx[i]] == '/':
+                        tmp_beam_3 = beam[3] + 1
+                    else:
+                        tmp_beam_3 = beam[3]
+                        
+                    beams.append([tmp_beam_0, tmp_beam_1, tmp_beam_2, tmp_beam_3])
+
+            b_cal = []
+            for beam in beams:
+                b_cal.append(cal_score(beam[1], beam[2], normalized))
+            top_beam_idx = np.argsort(b_cal)[-1*beam_size:]
+            beams = [beams[x] for x in top_beam_idx]
+
+        return beams[-1][0]
